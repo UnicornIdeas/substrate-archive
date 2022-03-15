76f0af2cd897b910172c23f97961268e60aa9498
helme <helmeseanualexandru@gmail.com>
2022-03-15 22:59:58 +0200

    tinkering

--- go-dictionary/go.mod
+++ go-dictionary/go.mod
@@ -2,4 +2,9 @@ module go-dictionary
 
 go 1.16
 
-require github.com/centrifuge/go-substrate-rpc-client/v4 v4.0.0 // indirect
+require (
+	github.com/centrifuge/go-substrate-rpc-client/v4 v4.0.0
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/itering/scale.go v1.1.49
+	github.com/itering/substrate-api-rpc v0.4.11
+)
--- go-dictionary/src/main.go
+++ go-dictionary/src/main.go
@@ -2,45 +2,136 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	gsrpc "github.com/centrifuge/go-substrate-rpc-client/v4"
-	"github.com/centrifuge/go-substrate-rpc-client/v4/types"
+	// "github.com/itering/substrate-api-rpc/websocket"
+	"github.com/gorilla/websocket"
+	"github.com/itering/substrate-api-rpc/rpc"
 )
 
-type MyEventRecords struct {
-	types.EventRecords
-	Scheduler_Scheduled []ParaInclusion_CandidateIncluded //nolint:stylecheck,golint
+type wsClient struct {
+	*websocket.Conn
+	currentIndex  int
+	maxIndexes    int
+	receiversList map[int]chan *rpc.JsonRpcResult
 }
 
-type ParaInclusion_CandidateIncluded struct {
-	Phase types.Phase
-	// snip
-	Topics []types.Hash
-}
+// func main() {
+// 	api, _ := gsrpc.NewSubstrateAPI("wss://polkadot.api.onfinality.io/public-ws")
+// 	hash, _ := api.RPC.Chain.GetBlockHash(9429341)
+
+// 	meta, _ := api.RPC.State.GetMetadata(hash)
+
+// 	fmt.Println(meta.AsMetadataV14.Pallets)
+// 	// fmt.Println(meta.AsMetadataV14.)
+
+// 	key, _ := types.CreateStorageKey(meta, "System", "Events", nil, nil)
+
+// 	fmt.Println(string(key))
+
+// 	rawEvents, _ := api.RPC.State.GetStorageRaw(key, hash)
+
+// 	fmt.Println(rawEvents)
+
+// 	// block, _ := api.RPC.Chain.GetBlock(hash)
+
+// 	// v, _ := api.RPC.State.GetRuntimeVersion(hash)
+
+// 	// fmt.Println(v.SpecName)
+// 	// fmt.Println(v.SpecVersion)
+
+// }
 
 func main() {
-	api, _ := gsrpc.NewSubstrateAPI("wss://polkadot.api.onfinality.io/public-ws")
-	hash, _ := api.RPC.Chain.GetBlockHash(100000)
+	endpoint := "wss://polkadot.api.onfinality.io/public-ws"
+	wsclient := InitWSClient(endpoint, 100)
 
-	meta, _ := api.RPC.State.GetMetadata(hash)
+	go wsclient.ReadWSMessages()
 
-	key, _ := types.CreateStorageKey(meta, "System", "Events", nil, nil)
+	for i := 0; i < 100; i++ {
+		_ = wsclient.GetBlockHash(i)
+		// fmt.Println(hash)
+	}
+	for {
+	}
 
-	fmt.Println(string(key))
+	// v := &rpc.JsonRpcResult{}
+	// websocket.SendWsRequest(nil, v, rpc.ChainGetBlockHash(1, 9429341))
 
-	// rawEvents, _ := api.RPC.State.GetStorageRaw(key, hash)
+	// hash, _ := v.ToString()
 
-	// events := MyEventRecords{} // Make sure this struct contains all the events that you care about
-	// err := types.EventRecordsRaw(*rawEvents).DecodeEventRecords(meta, &events)
+	// v2 := &rpc.JsonRpcResult{}
+	// websocket.SendWsRequest(nil, v2, rpc.ChainGetBlock(1, hash))
 
+	// meta := &rpc.JsonRpcResult{}
+	// err := websocket.SendWsRequest(nil, meta, rpc.StateGetMetadata(1, hash))
 	// fmt.Println(err)
-	// fmt.Println(events)
+	// // fmt.Println(meta)
 
-	// block, _ := api.RPC.Chain.GetBlock(hash)
+	// metaString, _ := meta.ToString()
+	// rawMeta := metadata.RuntimeRaw{Spec: 14, Raw: metaString}
+	// instant := metadata.Process(&rawMeta)
+	// fmt.Println(instant)
+}
 
-	// v, _ := api.RPC.State.GetRuntimeVersion(hash)
+// func DecodeEvents() {
+// 	m := scalecodec.MetadataDecoder{}
+// 	m.Init(utiles.HexToBytes(kusamaV14))
+// 	_ = m.Process()
+// 	c, err := ioutil.ReadFile(fmt.Sprintf("%s.json", "network/crab"))
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	types.RegCustomTypes(source.LoadTypeRegistry(c))
+// 	e := scalecodec.EventsDecoder{}
+// 	option := types.ScaleDecoderOption{Metadata: &m.Metadata}
 
-	// fmt.Println(v.SpecName)
-	// fmt.Println(v.SpecVersion)
+// 	eventRaw := "0x180000000000000080e36a090000000002000000010000000000000000000000000002000000020000000e022ac9219ace40f5846ed675dded4e25a1997da7eabdea2f78597a71d6f38031487089d481874e06bd4026807fd0464f5c7a1c691c21237ed9b912ac0443a2bc2200ca9a3b000000000000000000000000000002000000150600b4c4040000000000000000000000000000020000000e04b4f7f03bebc56ebe96bc52ea5ed3159d45a0ce3a8d7f082983c33ef133274747002d31010000000000000000000000000000020000000000401b5f1300000000000000"
+// 	e.Init(types.ScaleBytes{Data: utiles.HexToBytes(eventRaw)}, &option)
+// 	e.Process()
+// 	b, _ := json.Marshal(e.Value)
+// 	fmt.Println(string(b))
+// }
 
+func InitWSClient(endpoint string, maxIndexes int) *wsClient {
+	c, _, err := websocket.DefaultDialer.Dial(endpoint, nil)
+	if err != nil {
+		fmt.Println("dial:", err)
+		return nil
+	}
+	receiversMap := make(map[int]chan *rpc.JsonRpcResult)
+	for i := 0; i < maxIndexes; i++ {
+		responseChan := make(chan *rpc.JsonRpcResult)
+		receiversMap[i] = responseChan
+	}
+	return &wsClient{c, 1, maxIndexes, receiversMap}
+}
+
+func (c *wsClient) ReadWSMessages() {
+	v := &rpc.JsonRpcResult{}
+	for {
+		err := c.ReadJSON(v)
+		if err != nil {
+			log.Println("read:", err)
+			continue
+		}
+		fmt.Println(v)
+		// c.receiversList[v.Id] <- v
+	}
+}
+
+func (c *wsClient) GetBlockHash(blockHeight int) string {
+	c.WriteMessage(1, rpc.ChainGetBlockHash(c.currentIndex, blockHeight))
+
+	// response := <-c.receiversList[c.currentIndex]
+	c.currentIndex = (c.currentIndex + 1) % c.maxIndexes
+
+	// fmt.Println(response)
+	// blHash, err := response.ToString()
+	// if err != nil {
+	// 	return ""
+	// }
+
+	// return blHash
+	return ""
 }
