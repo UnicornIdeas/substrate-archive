8e8507996abdefdf3c61f29249ba8456dbcb3a09
helme <helmeseanualexandru@gmail.com>
2022-03-22 19:36:19 +0200

    rocksdb

--- go-dictionary/src/main.go
+++ go-dictionary/src/main.go
@@ -1,79 +1,33 @@
 package main
 
 import (
-	"go-dictionary/internal/downloader"
-	"log"
+	"encoding/hex"
+	"fmt"
+
+	"github.com/linxGnu/grocksdb"
 )
 
-// "github.com/itering/substrate-api-rpc/websocket"
-
-// func main() {
-// 	api, _ := gsrpc.NewSubstrateAPI("wss://polkadot.api.onfinality.io/public-ws")
-// 	hash, _ := api.RPC.Chain.GetBlockHash(9429341)
-
-// 	meta, _ := api.RPC.State.GetMetadata(hash)
-
-// 	fmt.Println(meta.AsMetadataV14.Pallets)
-// 	// fmt.Println(meta.AsMetadataV14.)
-
-// 	key, _ := types.CreateStorageKey(meta, "System", "Events", nil, nil)
-
-// 	fmt.Println(string(key))
-
-// 	rawEvents, _ := api.RPC.State.GetStorageRaw(key, hash)
-
-// 	fmt.Println(rawEvents)
-
-// 	// block, _ := api.RPC.Chain.GetBlock(hash)
-
-// 	// v, _ := api.RPC.State.GetRuntimeVersion(hash)
-
-// 	// fmt.Println(v.SpecName)
-// 	// fmt.Println(v.SpecVersion)
-
-// }
-
 func main() {
-	// endpoint := "wss://polkadot.api.onfinality.io/public-ws"
-	// wsClient, err := connection.InitWSClient(endpoint)
-	// if err != nil {
-	// 	log.Println(err)
-	// }
-	// go wsClient.ReadWSMessages()
+	opts := grocksdb.NewDefaultOptions()
+	opts.SetMaxOpenFiles(-1)
+	db, handles, _ := grocksdb.OpenDbAsSecondaryColumnFamilies(
+		opts,
+		"/tmp/rocksdb-polkadot/chains/polkadot/db/full",
+		"/tmp/rocksdb-secondary",
+		[]string{"default", "col0", "col1", "col2", "col3", "col4", "col5", "col6", "col7", "col8", "col9", "col10", "col11", "col12"},
+		[]*grocksdb.Options{opts, opts, opts, opts, opts, opts, opts, opts, opts, opts, opts, opts, opts, opts},
+	)
 
-	// internal.GetBlockHashes(wsClient, 100000)
+	//lookup key
+	// [(n >> 24) as u8, ((n >> 16) & 0xff) as u8, ((n >> 8) & 0xff) as u8, (n & 0xff) as u8]
+	resp, _ := db.GetCF(grocksdb.NewDefaultReadOptions(), handles[4], []byte{0, 0, 0, 1})
+	// resp, _ := db.GetCF(grocksdb.NewDefaultReadOptions(), handles[4], []byte{192, 9, 99, 88, 83, 78, 200, 210, 29, 1, 211, 75, 131, 110, 237, 71, 106, 28, 52, 63, 135, 36, 250, 33, 83, 220, 7, 37, 173, 121, 122, 144})
+	dataString := hex.EncodeToString(resp.Data())
+	fmt.Println(dataString)
 
-	downServer := downloader.InitDownServer("https://dot-rocksdb.polkashots.io/snapshot", "polkadot-9493950.RocksDb.tar.lz4")
-	// err := downServer.DownloadSnapshot()
-	// if err != nil {
-	// 	log.Println(err)
-	// }
+	// header, _ := db.GetCF(grocksdb.NewDefaultReadOptions(), handles[5], resp.Data())
+	// fmt.Println(string(header.Data()))
 
-	// err := downServer.DecompressSnapshot()
-	// if err != nil {
-	// 	log.Println(err)
-	// }
-
-	err := downServer.UntarSnapshot()
-	if err != nil {
-		log.Println(err)
-	}
-
-	// v := &rpc.JsonRpcResult{}
-	// websocket.SendWsRequest(nil, v, rpc.ChainGetBlockHash(1, 9429341))
-
-	// hash, _ := v.ToString()
-
-	// v2 := &rpc.JsonRpcResult{}
-	// websocket.SendWsRequest(nil, v2, rpc.ChainGetBlock(1, hash))
-
-	// meta := &rpc.JsonRpcResult{}
-	// err := websocket.SendWsRequest(nil, meta, rpc.StateGetMetadata(1, hash))
-	// fmt.Println(err)
-	// // fmt.Println(meta)
-
-	// metaString, _ := meta.ToString()
-	// rawMeta := metadata.RuntimeRaw{Spec: 14, Raw: metaString}
-	// instant := metadata.Process(&rawMeta)
-	// fmt.Println(instant)
+	body, _ := db.GetCF(grocksdb.NewDefaultReadOptions(), handles[6], resp.Data())
+	fmt.Println(body.Data())
 }
